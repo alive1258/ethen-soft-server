7a23f4bd1bf92c34be496c631e0c2cb153565a2a
unknown <mdomarfaruk149518@gmail.com>
2024-09-02 11:40:52 +0600

    [Done]login and refreshToken

--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -11,49 +11,55 @@ import { Secret } from "jsonwebtoken";
 import { ENUM_ROLE } from "../../../enums/user";
 import { User } from "../users/user.module";
 import { jwtHelpers } from "../../../helpers/jwtHelpers";
+import { TUser } from "../users/user.interface";
+import { Customer } from "../customers/customer.module";
 
-// login service function
+// Service to handle user login
 const loginUserService = async (
   loginData: TLoginUser
 ): Promise<TLoginUserResponse> => {
-  // destructured the login data
+  // Destructure email and password from the login data
   const { email, password } = loginData;
 
-  //   find the user
-  const isUserExist = await User.findOne(
+  // Find the user by email in both Customer and User collections
+  const customerData = await Customer.findOne(
     { email },
     { _id: 1, email: 1, password: 1, role: 1 }
   ).lean();
 
-  //   throw an error if the user is not exist
-  if (!isUserExist) {
+  const userData =
+    customerData ||
+    (await User.findOne(
+      { email },
+      { _id: 1, email: 1, password: 1, role: 1 }
+    ).lean());
+
+  // Throw an error if neither Customer nor User exists
+  if (!userData) {
     throw new ApiError(httpStatus.NOT_FOUND, "User does not exist.");
   }
 
-  //match the password
-  const isPasswordMatched = await bcrypt.compare(
-    password,
-    isUserExist.password
-  );
-  //   compare passwords
-  if (isUserExist.password && !isPasswordMatched) {
+  // Compare the provided password with the stored hashed password
+  const isPasswordMatched = await bcrypt.compare(password, userData.password);
+  if (!isPasswordMatched) {
     throw new ApiError(httpStatus.UNAUTHORIZED, "Password is incorrect.");
   }
 
-  // generating access token
+  // Generate an access token
   const accessToken = jwtHelpers.createToken(
-    { _id: isUserExist?._id, role: isUserExist.role },
+    { _id: userData._id, role: userData.role },
     config.jwt.secret as Secret,
     config.jwt.expires_in as string
   );
 
-  // generating refresh token
+  // Generate a refresh token
   const refreshToken = jwtHelpers.createToken(
-    { _id: isUserExist?._id, role: isUserExist.role },
+    { _id: userData._id, role: userData.role },
     config.jwt.refresh_secret as Secret,
     config.jwt.refresh_expires_in as string
   );
 
+  // Return the access and refresh tokens
   return {
     accessToken,
     refreshToken,
@@ -78,25 +84,30 @@ const refreshTokenService = async (
     throw new ApiError(httpStatus.FORBIDDEN, "Invalid Refresh Token");
   }
   //checking deleted user's refresh token
-  const { _id, role } = verifiedToken;
+  const { _id } = verifiedToken;
 
   //   set initial new accessToken variable
   let newAccessToken = "";
 
-  //   find the user from database
-  const isUserExist = await User.findOne(
+  //  try to find the customer data from database
+  const customerData = await Customer.findOne(
     { _id },
     { _id: 1, password: 1, role: 1 }
   ).lean();
 
+  //   if customer data is not available then find user data from database
+  const userData =
+    customerData ||
+    (await User.findOne({ _id }, { _id: 1, password: 1, role: 1 }).lean());
+
   // check the user is available or not
-  if (!isUserExist) {
+  if (!userData) {
     throw new ApiError(httpStatus.NOT_FOUND, "User does not exist.");
   }
 
   //generate new access token
   newAccessToken = jwtHelpers.createToken(
-    { _id: isUserExist._id, role: isUserExist.role },
+    { _id: userData._id, role: userData.role },
     config.jwt.secret as Secret,
     config.jwt.expires_in as string
   );
