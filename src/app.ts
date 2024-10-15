6d070a0bb2e5374b8b5ca79faeb7d0a966fe7f33
alive1258 <javashazib@gmail.com>
2024-10-15 11:48:52 +0600

    everything update today

--- src/app.ts
+++ src/app.ts
@@ -7,12 +7,11 @@ import cookieParser from "cookie-parser";
 
 const app: Application = express();
 
-// parsers update
-// update
+// everythis update now
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://ethensoft.com/",
     credentials: true,
   })
 );
