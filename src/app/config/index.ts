3f1baefa8a7bfd358291d152f311a2ce1f727d91
Md. Omar Faruk <mdomarfaruk149518@gmail.com>
2024-11-19 09:28:44 +0600

    [updated]payment

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -7,6 +7,7 @@ export default {
   env: process.env.NODE_ENV,
   port: process.env.PORT,
   database_url: process.env.DATABASE_URL,
+  production_url: process.env.PRODUCTION_URL,
   bcrypt_salt_rounds: process.env.BCRYPT_SALT_ROUNDS,
   jwt: {
     secret: process.env.JWT_SECRET,
